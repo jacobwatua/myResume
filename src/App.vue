881d26c020e0b5313e1bd4fa75993e64de0717f5
jacobwatua <jacobwatua254@gmai.com>
2023-04-24 13:59:48 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Resume />
+</template>
+
+<script>
+import Resume from "./components/resume.vue"
+export default {
+  components : {
+    Resume
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/resume.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="resume">
+        <div class="side-bar">
+            <div class="profile"><img src="../assets/images/profile.jpeg" alt="profile"></div>
+            <div class="summary">
+                <h3>Summary</h3>
+                <p>Experienced administrative assistant and customer service representative with strong organizational, communication, and problem-solving skills. Skilled in managing calendars, maintaining databases, handling customer inquiries, resolving complaints, and processing orders. Adept at multitasking, working independently or as part of a team, and adapting to new situations with ease. Committed to delivering top-notch results and exceeding expectations.</p>
+            </div>
+            <div class="skills">
+                <h3>Skills</h3>
+                <ul>
+                    <li>Communications management.</li>
+                    <li>Accounting skills.</li>
+                    <li>Back office operations.</li>
+                    <li>Schedule management.</li>
+                    <li>Tech-savvy.</li>
+                    <li>Solution development</li>
+                </ul>
+            </div>
+            <div class="other-skills">
+                <h3>Other Skills</h3>
+                <ul>
+                    <li>Python</li>
+                    <li>JavaScript</li>
+                    <li>HTML & CSS</li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <div class="header">
+                <h1 class="text-info">Jacob W.</h1>
+                <h1>Watua</h1>
+                <p class="phone"><img src="../assets/images/phone-call.png" alt="icon">+254792650720</p>
+                <p class="email"><img src="../assets/images/email.png" alt="icon">jacobwatua254@gmail.com</p>
+                <p class="address"><img src="../assets/images/location.png" alt="icon">00100, Nairobi, Kenya</p>
+            </div>
+            <h1 class="text-info">Experience</h1>
+            <div class="experience">
+                <div class="experienceHead">
+                    <p class="grey">07/2021-Current</p>
+                    <p class="role"><b>Administrative Assistant</b></p>
+                    <p class="company"><b>Mwangale & Co Advocates | Nairobi, Kenya</b></p>
+                </div>
+                <div class="experience-roles">
+                    <ul>
+                        <li>Answered and directed phone calls, emails, and other correspondence</li>
+                        <li>Scheduled appointments and meetings for executives and managers</li>
+                        <li>Maintained filing systems and databases</li>
+                        <li>Prepared reports, presentations, and other documents</li>
+                        <li>Coordinated travel arrangements for employees</li>
+                        <li>Managed office supplies and equipment inventory</li>
+                        <li>Assisted with onboarding new employees</li>
+                        <li>Demonstrated strong communication, organizational, and time management skills</li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+            <div class="experience">
+                <div class="experienceHead">
+                    <p class="grey">06/2020-05/2021</p>
+                    <p class="role"><b>Customer Service Representative</b></p>
+                    <p class="company"><b>Simply Interpreting | Bloomberg, USA</b></p>
+                </div>
+                <div class="experience-roles">
+                    <ul>
+                        <li>Responded to customer inquiries and resolved complaints via phone, email, and chat</li>
+                        <li>Processed orders and returns for customers</li>
+                        <li>Provided product information and recommendations to customers</li>
+                        <li>Maintained customer accounts and updated information in the company's database</li>
+                        <li>Collaborated with other departments to resolve customer issues</li>
+                        <li>Achieved a customer satisfaction rating of 95% based on customer surveys</li>
+                        <li>Demonstrated strong communication, problem-solving, and interpersonal skills</li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="eductation">
+                <h1 class="text-info">Education</h1>
+                <p class="grey">2021</p>
+                <p><b>Technical University of Mombasa | Mombasa Kenya</b></p>
+                <p>Bachelor of Business Administration: Accounting <br>Second Class Upper Division</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
